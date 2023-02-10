21548adeb243062f4d7f9ed5c31232234ad636cf
Md Shakil Zaman <mdshakilzaman2020@gmail.com>
2023-02-10 23:22:13 +0000

    first commit

--- src/main/java/BankService.java
+++ src/main/java/BankService.java
@@ -23,6 +23,7 @@ public class BankService {
      * @param amount the amount to be deposited.
      */
     public void deposit(double amount){
+        this.balance += amount;
 
     }
 
@@ -32,6 +33,11 @@ public class BankService {
      * @param amount the amount to be withdrawn.
      */
     public void withdraw(double amount){
+        if (amount > this.balance){
+            System.out.println("Not sufficient balance!");
+        }else{
+            this.balance -= amount;
+        }
 
     }
 
@@ -40,6 +46,6 @@ public class BankService {
      * @return the user's balance.
      */
     public double getBalance(){
-        return 0;
+        return this.balance;
     }
 }
